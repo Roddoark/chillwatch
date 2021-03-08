27147b6a0b1753d69547abed79694bbc30ec79f9
Ilyas Ghembaza <ilyas.ghembaza.ext@upply.com>
2021-03-08 22:40:05 +0100

    Ajout des assets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+
+import json from './data.json5';
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
