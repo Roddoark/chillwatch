8916bf3069d8fc46bbfd39ee1a1a2ec0a48927e7
Ilyas Ghembaza <ilyas.ghembaza.ext@upply.com>
2021-03-17 13:42:57 +0100

    Ajout du dossier dist/ dans le gitignore, ajout d'un dossier assets/, suppression du server (non nécessaire)

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <div id="main-element" className="title">Hello World !</div>
+  }
+}
+
+export default App;
---
+++ src/components/H1/h1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./h1.scss";
+
+export const H1 = () => {
+  return (
+    <h1>
+      Title
+    </h1>
+  )
+}
