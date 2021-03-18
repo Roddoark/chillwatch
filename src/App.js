11badf701d594d7ec69a8801846277d49df3e1c5
Ilyas Ghembaza <ilyas.ghembaza.ext@upply.com>
2021-03-18 13:04:15 +0100

    Ajout de composants

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
- 
+import "./style.scss"
+import H1 from "./components/H1/h1"
+
 const App = ({ title }) =>
-  <div>{title}</div>;
+  <div className="hellow">
+    <H1 />
+    {title}
+  </div>;
  
 export default App;
---
+++ src/components/H1/h1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./h1.scss";
+
+const H1 = () => {
+  return (
+    <h1 className="h1">
+      Titleeee
+    </h1>
+  )
+}
+
+export default H1;
